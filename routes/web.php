0a019a91f894d9103160b3713a774b9b2528f512
dietex1 <dietex13@gmail.com>
2025-06-03 14:13:59 +0200

    fixed create post (frogot about uncommenting)

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/u/{user:username}', [PublicProfileController::class, 'show'])->name
 Route::middleware('auth','verified')->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('dashboard');
     Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-//    Route::post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::post('/post', [PostController::class, 'store'])->name('post.store');
     Route::get('/{username}/{post:slug}',[PostController::class, 'show'])->name('post.show');
 
 });
