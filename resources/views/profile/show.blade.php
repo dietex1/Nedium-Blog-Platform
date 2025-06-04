159f9658b429f99c45a36d1d5d6ad6d85c575f15
dietex1 <dietex13@gmail.com>
2025-06-04 15:13:20 +0200

    added logic to show follow button if user is not same as authenticated user

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -24,15 +24,17 @@
                             <img src="{{ Storage::url('avatars/dummy.png') }}" alt="Default Avatar" class="w-20 h-20 rounded-full">
                         @endif
                         <h3 class="text-2xl font-bold">{{ $user->name }}</h3>
-                        <p class="text-gray-500"> 26k folowers </p>
+                        <p class="text-gray-500"> {{$user->followers()->count()}} folowers </p>
                         <p>
                             {{ $user->bio ?? 'No bio available.' }}
                         </p>
+                         @if($user->id !== auth()->id())
                         <div class="mt-4">
                             <button class="bg-emerald-600 rounded-full text-white px-4 py-2  hover:bg-emerald-700 transition-colors duration-300">
                                 Follow
                             </button>
                         </div>
+                         @endif
                     </div>
                 </div>
             </div>
